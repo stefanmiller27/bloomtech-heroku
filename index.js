9356e9cb95a497c0a2056ebc14a4bb938db47e0c
Stefan Miller <stefanmiller@Stefans-MacBook-Pro.local>
2022-03-22 00:15:27 -0400

    env port

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+require('dotenv').config();
+
+const PORT = process.env.PORT || 9000;
+
+
 const express = require('express');
 
 const server = express();
@@ -6,6 +11,7 @@ server.get('/', (req, res) => {
     res.end('<h1>hello world</h1>')
 });
 
-server.listen(9000, () => {
-    console.log('server is up on port 9000!')
+server.listen(PORT, () => {
+    console.log(process.env.MY_ENV);
+    console.log(`server is up on port ${PORT}, user is ${process.env.USER}!`)
 });
